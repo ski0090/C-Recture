83a0d84526d6b4233f638b8837b7e7cbaaa0d149
shalom <shalom92@atins.io>
2021-02-23 13:12:09 +0900

    2021 02 23 Demical And Conditional

--- 3강_과제/3강_과제/소스.cpp
+++ 3강_과제/3강_과제/소스.cpp
@@ -6,18 +6,26 @@ int main()
 	int Kor_recode = 0;
 	int Eng_recode = 0;
 	int Math_recode = 0;
+	int total_score = 0;
 
+	cout << "국어 점수 입력: ";
 	cin >> Kor_recode;
+	
+	cout << "영어 점수 입력: "; 
 	cin >> Eng_recode;
+	
+	cout << "수학 점수 입력: "; 
 	cin >> Math_recode;
+	
+	total_score = Kor_recode + Eng_recode + Math_recode;
+	float Aver = (float)total_score /3;
 
-	float Aver = (Kor_recode+Eng_recode+Math_recode)/3.0f;
-
+	cout << "##### Result #####" << endl; 
 	cout << "국어 점수:" << Kor_recode << endl;
 	cout << "영어 점수:" << Eng_recode << endl;
 	cout << "수학 점수:" << Math_recode << endl;
+	cout << "총점:" << total_score << endl;
 	cout << "평균:" << Aver << endl;
-
-
+	cout << "##################" << endl;
 	return 0;
 }
